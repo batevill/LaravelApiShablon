e39a4ef5466efaecde7aabc7966f5340c1320c30
batevill <dilshodjonabdumalikov3@gmail.com>
2024-09-17 12:36:48 +0500

    user seed update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,11 +10,10 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Create default user
         User::create([
             'name' => 'Default User',
             'email' => 'default@example.com',
-            'password' => Hash::make('password'), // default password
+            'password' => Hash::make('password'),
         ]);
     }
 }
